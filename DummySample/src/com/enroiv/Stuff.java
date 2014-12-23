254456490bf370c335bf3f31f150e345297f4de7
e4it1r0 <enroiv@gmail.com>
2014-12-23 15:23:18 -0800

    baby

--- DummySample/src/com/enroiv/Stuff.java
+++ DummySample/src/com/enroiv/Stuff.java
@@ -2,6 +2,7 @@ package com.enroiv;
 
 public class Stuff {
 	public static void main(String [] a){
-		System.out.println("KK");
+		System.out.println("KKK");
+		System.out.println("Took my baby away");
 	}
 }
